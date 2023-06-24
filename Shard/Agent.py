4fed78ce430bbf76eb197584e7fa9d8fd9a9fb3f
Fanouu <MileniumShad@gmail.com>
2023-06-24 12:45:55 +0200

    Initial commit

---
+++ Shard/Agent.py
@@ -0,0 +1,320 @@
+import json
+import os.path
+import os
+import pickle
+import sys
+from colorama import Fore, Style
+
+import numpy as np
+import tensorflow as tf
+import logging
+from json import JSONEncoder
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+def save_data(data):
+    with open('data/game.json', 'w') as file:
+        json.dump(data, file)
+
+
+def load_data():
+    if not os.path.exists('data/game.json'):
+        save_data({
+            "ALLAICOUP": 0,
+            "ALGOAICOUP": 0,
+            "win": [1, 1],
+            "wp": 0
+        })
+
+    with open('data/game.json', 'r') as file:
+        return json.load(file)
+
+
+X_train = []  # Liste pour stocker les états du jeu
+y_train = []  # Liste pour stocker les actions associées
+results = []
+recommended_action = []
+train = 0
+newlayer = 0
+rewards = []
+
+class Agent:
+
+    def generateModele(self):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(3, 3, 1)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation='relu'),
+            # tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(9, activation='softmax')
+        ])
+
+        if len(X_train) <= 0:
+            X_ = np.random.randint(0, 2, (2, 3, 3, 1))
+            y_ = np.random.randint(0, 2, (2, 9))
+            reward = np.array([0, 0])
+        else:
+            X_ = np.array(X_train)
+            y_ = np.array(self.convert_results_to_labels(recommended_action))
+            reward = np.array(rewards)
+
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        model.fit(X_, y_, sample_weight=reward, epochs=10, batch_size=32)
+
+        model.save('data/morpion_model')
+
+        return model
+
+    def __init__(self):
+        super(Agent, self).__init__()
+        global X_train, recommended_action
+        # self.load_data()
+        if not os.path.exists('data/morpion_model'):
+            print("no exists")
+            self.generateModele()
+
+        self.model = tf.keras.models.load_model('data/morpion_model')
+
+        print(self.model.summary())
+
+    def add_new_layer(self):
+        model = tf.keras.models.load_model('data/morpion_model')
+
+        model.layers[-2].trainable = False
+        name = "dense_{n}".format(n=len(model.layers))
+        new_layer = tf.keras.layers.Dense(64, activation='relu', name=name)
+        model.add(new_layer)
+        name = "out_{n}".format(n=len(model.layers))
+        output_layer = tf.keras.layers.Dense(9, activation='softmax', name=name)
+        model.add(output_layer)
+
+        if len(X_train) <= 0:
+            X_ = np.random.randint(0, 2, (2, 3, 3, 1))
+            y_ = np.random.randint(0, 2, (2, 9))
+            reward = np.array([0, 0])
+        else:
+            X_ = np.array(X_train)
+            y_ = np.array(self.convert_results_to_labels(recommended_action))
+            reward = np.array(rewards)
+
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        model.fit(X_, y_, sample_weight=reward, epochs=10, batch_size=32)
+
+        self.model = model
+        model.save('data/morpion_model')
+        print(f"{Fore.RED}NOUVELLE LAYER DENSE !{Style.RESET_ALL}")
+
+    def load_data(self):
+        global X_train, recommended_action
+        if os.path.exists('data/training_data.pkl'):
+            with open('data/training_data.pkl', 'rb') as f:
+                data = pickle.load(f)
+                X_train, recommended_action = data['X_train'], data['recommended_action']
+
+    def save_data(self):
+        global X_train, recommended_action
+        data = {'X_train': X_train, 'recommended_action': recommended_action}
+        with open('data/training_data.pkl', 'wb') as f:
+            pickle.dump(data, f)
+
+    def train(self):
+        global train
+        data = load_data()
+        WIN = data["win"]
+        print(WIN[1])
+        if WIN[1] == 3000:
+            WP = data["wp"]
+            data = {"ALLAICOUP": 0, "ALGOAICOUP": 0, "win": [1, 1], "wp": WP}
+            save_data(data)
+            self.add_new_layer()
+
+        if train == 70:
+            model = tf.keras.models.load_model('data/morpion_model')
+            X_new = np.array(X_train)
+            y_new = np.array(self.convert_results_to_labels(recommended_action))
+            reward = np.array(rewards)
+            model.fit(X_new, y_new, sample_weight=reward, epochs=12, batch_size=32)
+            model.save('data/morpion_model')
+            self.model = model
+            print(f"{Fore.GREEN}Le modèle a été mis à jour avec de nouvelles données.{Style.RESET_ALL}")
+            train = 0
+            reset()
+            return
+        train = train + 1
+
+    def prediction(self, etat_jeu):
+        correct = self.check(etat_jeu)
+        newetat = np.expand_dims(etat_jeu, axis=0)
+        prediction = self.model.predict(newetat)
+        prediction = prediction[0]
+        prediction = [x if x != -1 else float('-inf') for x in prediction]
+        action = self.epsilon_greedy_action(prediction, 0.1)
+
+        target = [0] * 9
+        for recommend, level in correct:
+            target[recommend] = level
+        ag = np.argmax(target)
+        if ag != action and target[ag] == 3:
+            target = []
+            for i in range(9):
+                if i == ag:
+                    target.append((i, 3))
+                else:
+                    target.append((i, -1))
+            X_train.append(etat_jeu)
+            recommended_action.append(target)
+            rewards.append(1.0)
+        return action
+
+    def determine_reward(self, result):
+        if result == 3:
+            return 1.0  # victoire
+        elif result == 2:
+            return 0.5 #strate
+        elif result == 1:
+            return 0.1  # tactic
+        else:
+            return 0.0  # default
+
+    def epsilon_greedy_action(self, prediction, epsilon):
+        if np.random.uniform(0, 1) < epsilon:
+            return np.random.randint(len(prediction))
+        else:
+            return np.argmax(prediction)
+
+    def save(self, etat_jeu, oldetat_jeux):
+        X_train.append(etat_jeu)
+        check = self.check(oldetat_jeux)
+        recommended_action.append(check)  # ATTENTION SI ALGO CHELOU REMETTRE OLD
+
+        result = 0.0
+        for etat in self.convert_results_to_labels([check]):
+            for i in etat:
+                determine = self.determine_reward(i)
+                if determine == 1.0:
+                    result = 1.0
+                    break
+                elif determine > result:
+                    result = determine
+        rewards.append(result)
+
+        self.save_data()
+
+    def play(self, etat_jeu):
+        action_AI = self.prediction(etat_jeu)
+        # print("L'IA joue à la position :", action_AI)
+        ligne, colnone = positionToXY(action_AI)
+        old_etat = etat_jeu
+        etat_jeu[ligne][colnone] = 1
+
+        return [etat_jeu, ligne, colnone, old_etat]
+
+    def check(self, oldetat_jeu):
+        expected = []
+        i = 0
+        victory = False
+        for ligne, v in enumerate(oldetat_jeu):
+            for colonne, value in enumerate(v):
+                if value == 1 or value == 2:
+                    expected.append((i, -1))
+                else:
+                    possible_win = self.possible_win(oldetat_jeu, i)
+                    if possible_win == 1:
+                        if not victory:
+                            expected.append((i, 3))
+                            victory = True
+                    elif possible_win == 2:
+                        if not victory:
+                            expected.append((i, 2))
+
+                    else:
+                        expected.append((i, 1))
+
+                i = i + 1
+
+        return expected
+
+    def possible_win(self, grille, action):
+        ligne, colonne = positionToXY(action)
+        if grille[ligne][colonne] == 0:
+            grille[ligne][colonne] = 1
+            if self.determine_result(grille) == 1:
+                grille[ligne][colonne] = 0
+                return 1  # ELLE PEUT GAGNER
+            grille[ligne][colonne] = 0
+
+        if grille[ligne][colonne] == 0:
+            grille[ligne][colonne] = 2
+            if self.determine_result(grille) == 2:
+                grille[ligne][colonne] = 0
+                return 2  # L ALGO PEUT GAGNER
+            grille[ligne][colonne] = 0
+
+        return 0
+
+    def determine_result(self, grille):
+        for ligne in range(3):
+            if grille[ligne][0] == grille[ligne][1] == grille[ligne][2] == 2:
+                return 2  # l'ALGO gagne
+            elif grille[ligne][0] == grille[ligne][1] == grille[ligne][2] == 1:
+                return 1  # L'AI gagne
+
+        for colonne in range(3):
+            if grille[0][colonne] == grille[1][colonne] == grille[2][colonne] == 2:
+                return 2
+            elif grille[0][colonne] == grille[1][colonne] == grille[2][colonne] == 1:
+                return 1
+
+        if grille[0][0] == grille[1][1] == grille[2][2] == 2:
+            return 2
+        elif grille[0][0] == grille[1][1] == grille[2][2] == 1:
+            return 1
+        if grille[0][2] == grille[1][1] == grille[2][0] == 2:
+            return 2
+        elif grille[0][2] == grille[1][1] == grille[2][0] == 1:
+            return 1
+
+        return 0
+
+    def convert_results_to_labels(self, recommended_action):
+        labels = []
+        for i in range(len(recommended_action)):
+            target = [0] * 9
+            for recommend, level in recommended_action[i]:
+                target[recommend] = level
+            labels.append(target)
+        return np.array(labels)
+
+
+def positionToXY(position_AI):
+    ligne, colonne = position_AI // 3, position_AI % 3
+    return ligne, colonne
+
+def XYToPosition(ligne, colonne):
+    position_AI = ligne * 3 + colonne
+    return position_AI
+
+
+def reset():
+    global X_train, y_train, recommended_action
+    X_train = []
+    recommended_action = []
+
+
+agent = Agent()
+etat_jeu = np.array([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+etat_jeu = agent.play(etat_jeu)
+print(etat_jeu)
+
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
---
+++ Shard/TestAI.py
@@ -0,0 +1,302 @@
+import json
+import os
+
+import pygame
+import sys
+import random
+import Agent
+from colorama import Fore, Style
+
+pygame.init()
+
+largeur = 600
+hauteur = 600
+
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+BLEU = (0, 0, 255)
+
+fenetre = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Jeu de Morpion")
+
+taille_case = largeur // 3
+
+grille = [[None, None, None],
+          [None, None, None],
+          [None, None, None]]
+
+def save_data(data):
+    with open('data/game.json', 'w') as file:
+        json.dump(data, file)
+
+
+def load_data():
+    if not os.path.exists('data/game.json'):
+        save_data({
+            "ALLAICOUP": 0,
+            "ALGOAICOUP": 0,
+            "win": [1, 1],
+            "wp": 0
+        })
+
+    with open('data/game.json', 'r') as file:
+        return json.load(file)
+
+
+loaded_data = load_data()
+print(loaded_data)
+WP = loaded_data["wp"]
+
+# Joueur actuel (1 ou 2)
+joueur = 1
+starter = 2
+
+AICOUP = 0
+DLCOUP = 0
+SAICOUP = loaded_data["ALLAICOUP"]
+SDLCOUP = loaded_data["ALGOAICOUP"]
+
+WIN = loaded_data["win"]
+
+agent = Agent.Agent()
+
+
+def start():
+    global starter
+    if starter == 1:
+        return 2
+    else:
+        return 1
+
+
+def etat_jeu():
+    etat_jeux = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]]
+    for i, v in enumerate(grille):
+        for ii, vv in enumerate(v):
+            if vv == "X":
+                vv = 2
+            elif vv == "O":
+                vv = 1
+            else:
+                vv = 0
+
+            etat_jeux[i][ii] = vv
+    return etat_jeux
+
+
+def load_grille():
+    for ligne in range(3):
+        for colonne in range(3):
+            x = colonne * taille_case + taille_case // 2
+            y = ligne * taille_case + taille_case // 2
+            symbole = grille[ligne][colonne]
+
+            if symbole == "X":
+                pygame.draw.line(fenetre, NOIR, (x - 50, y - 50), (x + 50, y + 50), 4)
+                pygame.draw.line(fenetre, NOIR, (x + 50, y - 50), (x - 50, y + 50), 4)
+            elif symbole == "O":
+                pygame.draw.circle(fenetre, NOIR, (x, y), 50, 4)
+
+
+def check_victory(symbole):
+    for ligne in range(3):
+        if grille[ligne][0] == grille[ligne][1] == grille[ligne][2] == symbole:
+            return True
+
+    for colonne in range(3):
+        if grille[0][colonne] == grille[1][colonne] == grille[2][colonne] == symbole:
+            return True
+
+    if grille[0][0] == grille[1][1] == grille[2][2] == symbole:
+        return True
+    if grille[0][2] == grille[1][1] == grille[2][0] == symbole:
+        return True
+
+    return False
+
+
+def rectifieAI():
+    for ligne in range(3):
+        for colonne in range(3):
+            if grille[ligne][colonne] is None:
+                grille[ligne][colonne] = "O"
+                if check_victory("O"):
+                    grille[ligne][colonne] = None
+                    return ligne, colonne
+                grille[ligne][colonne] = None
+
+    for ligne in range(3):
+        for colonne in range(3):
+            if grille[ligne][colonne] is None:
+                grille[ligne][colonne] = "X"
+                if check_victory("X"):
+                    grille[ligne][colonne] = None
+                    return ligne, colonne
+                grille[ligne][colonne] = None
+
+    coups_possibles = []
+    for ligne in range(3):
+        for colonne in range(3):
+            if grille[ligne][colonne] is None:
+                coups_possibles.append((ligne, colonne))
+    return random.choice(coups_possibles)
+
+
+def choisir_coup_IA2():
+    #if random.randint(0, 1) == 1:
+        for ligne in range(3):
+            for colonne in range(3):
+                if grille[ligne][colonne] is None:
+                    grille[ligne][colonne] = "X"
+                    if check_victory("X"):
+                        grille[ligne][colonne] = None
+                        return ligne, colonne
+                    grille[ligne][colonne] = None
+
+        for ligne in range(3):
+            for colonne in range(3):
+                if grille[ligne][colonne] is None:
+                    grille[ligne][colonne] = "O"
+                    if check_victory("O"):
+                        grille[ligne][colonne] = None
+                        return ligne, colonne
+                    grille[ligne][colonne] = None
+
+        coups_possibles = []
+        for ligne in range(3):
+            for colonne in range(3):
+                if grille[ligne][colonne] is None:
+                    coups_possibles.append((ligne, colonne))
+
+        return random.choice(coups_possibles)
+    #else:
+    #    coups_possibles = []
+    #    for ligne in range(3):
+    #       for colonne in range(3):
+    #            if grille[ligne][colonne] is None:
+    #                coups_possibles.append((ligne, colonne))
+    #
+    #    return random.choice(coups_possibles)
+
+
+while True:
+    for evenement in pygame.event.get():
+        if evenement.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    if not check_victory("X") and not check_victory("O"):
+        if joueur == 1 and not check_victory("O") and not all(None not in ligne for ligne in grille):
+            ligne, colonne = choisir_coup_IA2()
+            old = etat_jeu()
+            grille[ligne][colonne] = "X"
+            joueur = 2
+
+        if check_victory("X"):
+
+            print("{LIGHTRED_EX}Victoire de l'{GREEN}Algo {RESET_ALL}".format(LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+            print("{YELLOW}L'AI a mis un certain nombre independant de COUP: {CYAN} {AICOUP}/{DLCOUP}{YELLOW}({CYAN} {PERCENT}%{YELLOW}) {RESET_ALL}".format(AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, PERCENT=(AICOUP / DLCOUP) * 100, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+            AICOUP = 0
+            DLCOUP = 0
+            WIN[1] = WIN[1] + 1
+            total = WIN[0] + WIN[1]
+            save_data({
+                "ALLAICOUP": SAICOUP,
+                "ALGOAICOUP": SDLCOUP,
+                "win": WIN,
+                "wp": WP
+            })
+            print("{YELLOW} Elle a aussi gagner {LIGHTGREEN_EX} {WIN}{YELLOW} partie(s) sur {LIGHTRED_EX}{total}{YELLOW}({CYAN}{PERCENT}{YELLOW}%) \nEt sur toute c'est partie {CYAN}{PERCENT2}% {YELLOW}sont mis par l'IA{RESET_ALL}".format(LIGHTGREEN_EX=Fore.LIGHTGREEN_EX, PERCENT2=(SAICOUP / SDLCOUP) * 100, PERCENT=(WIN[0] / total) * 100, total=WIN[1], WIN=WIN[0], AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+
+            agent.train()
+            pygame.draw.line(fenetre, BLEU, (0, taille_case // 2), (largeur, taille_case // 2), 4)
+            pygame.display.update()
+            #pygame.time.wait(2000)
+            grille = [[None, None, None], [None, None, None], [None, None, None]]
+            starter = start()
+            joueur = starter
+        elif all(None not in ligne for ligne in grille):
+
+            print("{WHITE}Egaliter {RESET_ALL}".format(WHITE=Fore.WHITE, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+            print("{YELLOW}L'AI a mis un certain nombre independant de COUP: {CYAN} {AICOUP}/{DLCOUP}{YELLOW}({CYAN} {PERCENT}%{YELLOW}) {RESET_ALL}".format(AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, PERCENT=(AICOUP / DLCOUP) * 100, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+            AICOUP = 0
+            DLCOUP = 0
+            total = WIN[0] + WIN[1]
+            save_data({
+                "ALLAICOUP": SAICOUP,
+                "ALGOAICOUP": SDLCOUP,
+                "win": WIN,
+                "wp": WP
+            })
+            print("{YELLOW} Elle a aussi gagner {LIGHTGREEN_EX} {WIN}{YELLOW} partie(s) sur {LIGHTRED_EX}{total}{YELLOW}({CYAN}{PERCENT}{YELLOW}%) \nEt sur toute c'est partie {CYAN}{PERCENT2}% {YELLOW}sont mis par l'IA{RESET_ALL}".format(LIGHTGREEN_EX=Fore.LIGHTGREEN_EX, PERCENT2=(SAICOUP / SDLCOUP) * 100, PERCENT=(WIN[0] / total) * 100, total=WIN[1], WIN=WIN[0], AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+
+            agent.train()
+            pygame.display.update()
+            #pygame.time.wait(2000)
+            grille = [[None, None, None], [None, None, None], [None, None, None]]
+            starter = start()
+            joueur = starter
+        else:
+            play = agent.play(etat_jeu())
+            case = grille[play[1]][play[2]]
+            DLCOUP = DLCOUP + 1
+            SDLCOUP = SDLCOUP + 1
+            action = Agent.XYToPosition(play[1], play[2])
+            if case == None:
+                grille[play[1]][play[2]] = "O"
+                AICOUP = AICOUP + 1
+                SAICOUP = SAICOUP + 1
+            else:
+                ligne, colonne = rectifieAI()
+                action = Agent.XYToPosition(ligne, colonne)
+                grille[ligne][colonne] = "O"
+
+            #agent.save(etat_jeu(), play[3])
+            agent.save(play[3], play[3])
+            agent.save(etat_jeu(), etat_jeu())
+            joueur = 1
+            if check_victory("O"):
+
+                print("{LIGHTGREEN_EX}Victoire de l'{RED}IA {RESET_ALL}".format(LIGHTGREEN_EX=Fore.LIGHTGREEN_EX, RED=Fore.RED, WHITE=Fore.WHITE, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+                print("{YELLOW}L'AI a mis un certain nombre independant de COUP: {CYAN} {AICOUP}/{DLCOUP}{YELLOW}({CYAN} {PERCENT}%{YELLOW}) {RESET_ALL}".format(AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, PERCENT=(AICOUP / DLCOUP) * 100, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+                WIN[0] = WIN[0] + 1
+                total = WIN[0] + WIN[1]
+                if ((AICOUP / DLCOUP) * 100) >= 100:
+                    WP = WP + 1
+                AICOUP = 0
+                DLCOUP = 0
+                save_data({
+                    "ALLAICOUP": SAICOUP,
+                    "ALGOAICOUP": SDLCOUP,
+                    "win": WIN,
+                    "wp": WP
+                })
+                print("{YELLOW} Elle a aussi gagner {LIGHTGREEN_EX} {WIN}{YELLOW} partie(s) sur {LIGHTRED_EX}{total}{YELLOW}({CYAN}{PERCENT}{YELLOW}%) \nEt sur toute c'est partie {CYAN}{PERCENT2}% {YELLOW}sont mis par l'IA{RESET_ALL}".format(LIGHTGREEN_EX=Fore.LIGHTGREEN_EX, PERCENT2=(SAICOUP / SDLCOUP) * 100, PERCENT=(WIN[0] / total) * 100, total=WIN[1], WIN=WIN[0], AICOUP=AICOUP, DLCOUP=DLCOUP, CYAN=Fore.CYAN, YELLOW=Fore.YELLOW, LIGHTRED_EX=Fore.LIGHTRED_EX, GREEN=Fore.GREEN, RESET_ALL=Style.RESET_ALL))
+
+                agent.train()
+
+                fenetre.fill(BLANC)
+                for ligne in range(1, 3):
+                    pygame.draw.line(fenetre, NOIR, (0, ligne * taille_case), (largeur, ligne * taille_case), 4)
+                for colonne in range(1, 3):
+                    pygame.draw.line(fenetre, NOIR, (colonne * taille_case, 0),
+                                     (colonne * taille_case, hauteur), 4)
+                load_grille()
+                pygame.display.update()
+                #pygame.time.wait(2000)
+
+                grille = [[None, None, None], [None, None, None], [None, None, None]]
+                starter = start()
+                joueur = starter
+
+    fenetre.fill(BLANC)
+
+    for ligne in range(1, 3):
+        pygame.draw.line(fenetre, NOIR, (0, ligne * taille_case), (largeur, ligne * taille_case), 4)
+    for colonne in range(1, 3):
+        pygame.draw.line(fenetre, NOIR, (colonne * taille_case, 0), (colonne * taille_case, hauteur), 4)
+
+    load_grille()
+
+    pygame.display.update()
